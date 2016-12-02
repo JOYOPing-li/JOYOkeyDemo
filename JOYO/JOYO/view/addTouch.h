9f256950fe54afd50587144ed8d53cba252d7295
tarena07 <1043067675@qq.com>
2016-12-02 17:38:01 +0800

    View

---
+++ JOYO/JOYO/view/addTouch.h
@@ -0,0 +1,13 @@
+//
+//  addTouch.h
+//  JOYO
+//
+//  Created by tarena07 on 2016/12/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface addTouch : UIView
+
+@end
---
+++ JOYO/JOYO/view/addTouch.m
@@ -0,0 +1,21 @@
+//
+//  addTouch.m
+//  JOYO
+//
+//  Created by tarena07 on 2016/12/2.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import "addTouch.h"
+
+@implementation addTouch
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
